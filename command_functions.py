fbae79a411687b9cb8673cd9e4aeb570c726e75e
kin10101 <ijoacquin@gmail.com>
2023-04-23 14:19:25 +0800

    mapped new functions

--- command_functions.py
+++ command_functions.py
@@ -2,7 +2,8 @@ import playaudio
 import texttospeech as tts
 import datetime
 from datetime import datetime
-
+import addintent
+import train
 
 def sing():
     print("singing")
@@ -28,11 +29,25 @@ def get_date():
     tts.speak("Today is " + current_day)
 
 
+def add_intents():
+    # add new intents to the intents.json file
+    addintent.add_intent()
+
+    pass
+
+def train_bot():
+    # train the bot with the new intents
+    train.trainBot()
+    pass
+
 # convert to a json file afterwards
+
 
 command_mappings = {
     "test_func": test_func,
     "sing": sing,
     "current_time": get_time,
-    "current_date": get_date
+    "current_date": get_date,
+    "add_intents": add_intents,
+    "update": train_bot
 }
